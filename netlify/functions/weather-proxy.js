d1e6136e31f2869f70637fd8b93ed98dbcdc39b5
chenhanktaiwan <hankchens@gmail.com>
2025-07-18 12:14:17 +0800

    Update weather-proxy.js

--- netlify/functions/weather-proxy.js
+++ netlify/functions/weather-proxy.js
@@ -1,18 +1,15 @@
 // /netlify/functions/weather-proxy.js
 
 exports.handler = async function (event, context) {
-  // 【已修改】從環境變數讀取新的 OpenWeatherMap API 金鑰
+  // 從環境變數讀取 OpenWeatherMap API 金鑰
   const OWM_API_KEY = process.env.OPENWEATHERMAP_API_KEY;
 
-  // 【已修改】使用臺北市的經緯度
+  // 使用臺北市的經緯度
   const lat = '25.0478'; // 臺北緯度
   const lon = '121.5171'; // 臺北經度
   
-  // 【已修改】OpenWeatherMap One Call API 3.0 的 URL
-  // exclude=current,minutely,hourly,alerts -> 我們只需要每日預報 (daily)
-  // units=metric -> 單位使用攝氏度
-  // lang=zh_tw -> 語言使用繁體中文
-  const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${OWM_API_KEY}&units=metric&lang=zh_tw`;
+  // 【已修改】更換為 5-day/3-hour Forecast API，這個 API 包含在預設免費方案中
+  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${OWM_API_KEY}&units=metric&lang=zh_tw`;
 
   try {
     const fetch = require('node-fetch');
